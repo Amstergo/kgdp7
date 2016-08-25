ae9592e1eab5924a8ed9f305c1bb2d104114f795
John <amstergo46@gmail.com>
2016-08-25 23:14:37 +0300

    add routes

--- kgdp.js
+++ kgdp.js
@@ -12,29 +12,25 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('kgdp7');
+  res.render('home');
 });
 app.get('/about', function(req, res){
-  res.type('text/plain');
-  res.send('O kgdp7');
+  res.render('about');
 });
 
 // users 404
 
 app.use(function(req, res){
-  res.type('text-plain');
   res.status(404);
-  res.send('404 page not found');
+  res.render('404');
 });
 
 // users 500
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - error server');
+  res.render('500');
 });
 app.listen(app.get('port'), function(){
 
